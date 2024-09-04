5882df696ed3a43a4c5f0f942d10d22a8461da67
Ridzz110 <84570922+Ridzz110@users.noreply.github.com>
2024-09-04 09:38:16 +0500

    some fixes

--- app/component/main.tsx
+++ app/component/main.tsx
@@ -156,7 +156,7 @@ export default function Home() {
         <section className='h-screen w-full flex flex-col bg-black justify-center items-center'>
           <h1 className='bg-gradient-to-r from-purple-300 to-blue-200 to bg-yellow-100 inline-block text-transparent bg-clip-text lg:text-6xl text-5xl  font-semibold'>Hello! This is Rida.</h1>
           <h2 className='font-poppins text-center text-white mt-8 text-lg w-3/4 font-light tracking-wide'>
-            An aspiring AI Engineer and Software Development student. I do Web Development and I love to design clean UI layouts. My projects are built on core concepts of each language I'm learning. Explore them below.
+            An aspiring AI Engineer and Software Development student. I do Web Development and I love to design clean UI layouts. My projects are built on core concepts of each language I&#39;m learning. Explore them below.
           </h2>
         </section>
 
